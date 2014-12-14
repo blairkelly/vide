473a7bdd754343f2f35c8549f80e4716b75c28cf
Blair Kelly <blairkelly1@gmail.com>
2014-12-14 13:44:13 -0500

    small change

--- views/index/route.js
+++ views/index/route.js
@@ -2,7 +2,6 @@ var app = module.parent.exports.app;
 var sport = module.parent.exports.serialcoms;
 
 sport.on('data', function (data) {
-    
     var pairs = data.split('&');
     var pieces = null;
     var params = {};
@@ -10,9 +9,7 @@ sport.on('data', function (data) {
         pieces = pairs[i].split('=');
         params[pieces[0]] = pieces[1];
     }
-
     console.log(params);
-    
 });
 
 app.get('/', function (req, res) {
